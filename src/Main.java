2a96c3f4d9548bcf81c4f137b5cd5aa22a150540
= <=>
2020-12-19 12:34:05 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main {
+
+    public static int FirstFactorial(int num) {
+        // code goes here
+        if (num == 1) return 1;
+        return num * FirstFactorial(num - 1);
+    }
+
+    public static void main(String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        System.out.print(FirstFactorial(s.nextInt()));
+    }
+
+}
